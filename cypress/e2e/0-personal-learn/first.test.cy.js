b6f9a3bbcc6981d1f9fcfe739f3ee4b05d0b63d5
Putra <80143004+MULKHI@users.noreply.github.com>
2022-08-11 13:28:29 +0700

    Update first.test.cy.js

--- cypress/e2e/0-personal-learn/first.test.cy.js
+++ cypress/e2e/0-personal-learn/first.test.cy.js
@@ -17,23 +17,21 @@ describe('My First Test', () => {
         //membuat pernyataan bahwa sudah memasuki halaman yang benar(Make an assertion)
         cy.url().should('include', '/commands/actions')
 
-        //melakukan input pada kolom email, password, fullname,descripion adn couponCode
+         //melakukan input pada kolom email, password, fullname,descripion adn couponCode
+        cy.get('#email1').clear()
         cy.get('#email1')
             .type('fake@email.com')
             .should('have.value', 'fake@email.com')
 
+        cy.get('#password1').clear()
         cy.get('#password1')
             .type('password123')
             .should('have.value', 'password123')
 
-        cy.get('#fullName1')
-            .type('Mulkhi Putra Lesmana')
-
-        cy.get('#description')
-            .type("this my first time using Cypress and it's so pretty good I like it")
-
-        cy.get('#couponCode1')
-            .type('666999')
+        cy.get('#fullName1').type('Mulkhi Putra Lesmana')
+        cy.get('#description').type("this my first time using Cypress and it's so pretty good I like it")
+        cy.get('#couponCode1').type('666999')
+        cy.get('button').contains('Submit').click()
 
 
         
